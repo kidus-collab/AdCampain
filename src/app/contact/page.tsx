fb12e5fd6fb9784588b4add6d043790f0777fce9
kidus-collab <guyfreaky894@gmail.com>
2023-11-03 22:09:49 +0300

    route added

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const contact = () => {
+  return (
+    <main>
+      <h1>Contact page</h1>
+    </main>
+  );
+};
+
+export default contact;
---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const pricing = () => {
+  return (
+    <main>
+      <h1>Pricing page</h1>
+    </main>
+  );
+};
+
+export default pricing;
