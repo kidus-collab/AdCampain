a8eb03bb2e270dc3714577da853037406b852973
kidus-collab <guyfreaky894@gmail.com>
2023-11-04 23:26:07 +0300

    style imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
+//import Image from "next/image";
 import styles from "./page.module.css";
+import { Box, Stack } from "@mantine/core";
 
 export default function Home() {
   return (
