a7995056979d20baa8e495fe4d0ce101174dc601
kidus-collab <guyfreaky894@gmail.com>
2023-11-07 21:45:36 +0300

    Component building

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 
+import { Box, Text, Button, Image, Group, Container } from "@mantine/core";
+
+import { Font, images, color } from "../../utils/constants";
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Group justify="space-between">
+      <Container fluid>
+        <Box ml={3}>
+          <Image src={images.logo} />
+        </Box>
+        <Box display={{ base: "none", md: "flex" }}>
+          <Box>
+            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
+              pricing
+            </Text>
+          </Box>
+          <Box>
+            {" "}
+            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
+              About
+            </Text>
+          </Box>
+          <Box>
+            {" "}
+            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
+              Services
+            </Text>
+          </Box>
+          <Box>
+            {" "}
+            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
+              Our work
+            </Text>
+          </Box>
+        </Box>
+
+        <Button bg={color[0]}>Contact Us</Button>
+      </Container>
+    </Group>
+  );
 };
 
 export default NavBar;
