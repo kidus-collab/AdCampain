c9b1fb780fbeb50ddba734c4b30e3f4da0f4a5e5
kidus-collab <guyfreaky894@gmail.com>
2023-11-08 22:44:31 +0300

    feat(mod) : building

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -1,44 +1,55 @@
 import React from "react";
 
-import { Box, Text, Button, Image, Group, Container } from "@mantine/core";
+import { Box, Text, Button, Flex, Container } from "@mantine/core";
+import Image from "next/image";
 
-import { Font, images, color } from "../../utils/constants";
+import { Font, images, color } from "../utils/constants";
 const NavBar = () => {
   return (
-    <Group justify="space-between">
-      <Container fluid>
-        <Box ml={3}>
-          <Image src={images.logo} />
+    <Flex
+      mx={6}
+      pos="absolute"
+      top={5}
+      left={0}
+      right={0}
+      justify="space-between"
+    >
+      <Box ml={3}>
+        <Image
+          src={images.logo}
+          alt="Website Logo"
+          width="100px"
+          height="200px"
+        />
+      </Box>
+      <Box display={{ base: "none", md: "flex" }}>
+        <Box>
+          <Text ff={Font[0]} fs={{ md: "14px", lg: "16px" }} fw="bold">
+            pricing
+          </Text>
         </Box>
-        <Box display={{ base: "none", md: "flex" }}>
-          <Box>
-            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
-              pricing
-            </Text>
-          </Box>
-          <Box>
-            {" "}
-            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
-              About
-            </Text>
-          </Box>
-          <Box>
-            {" "}
-            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
-              Services
-            </Text>
-          </Box>
-          <Box>
-            {" "}
-            <Text ff={Font} fs={{ md: "14px", lg: "16px" }} fw="bold">
-              Our work
-            </Text>
-          </Box>
+        <Box>
+          {" "}
+          <Text ff={Font[0]} fs={{ md: "14px", lg: "16px" }} fw="bold">
+            About
+          </Text>
         </Box>
+        <Box>
+          {" "}
+          <Text ff={Font[0]} fs={{ md: "14px", lg: "16px" }} fw="bold">
+            Services
+          </Text>
+        </Box>
+        <Box>
+          {" "}
+          <Text ff={Font[0]} fs={{ md: "14px", lg: "16px" }} fw="bold">
+            Our work
+          </Text>
+        </Box>
+      </Box>
 
-        <Button bg={color[0]}>Contact Us</Button>
-      </Container>
-    </Group>
+      <Button bg={color[0]}>Contact Us</Button>
+    </Flex>
   );
 };
 
