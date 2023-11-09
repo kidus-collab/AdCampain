cd5794b7f24323649105cfbc053e55185a2e34a8
kidus-collab <guyfreaky894@gmail.com>
2023-11-09 21:29:49 +0300

    Next ui customization

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [nextui()],
+};
