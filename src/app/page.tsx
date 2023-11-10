d1abf50f5d85d4ce1dc136981b856f5aea94c60e
kidus-collab <guyfreaky894@gmail.com>
2023-11-10 21:39:45 +0300

    added to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+"use client";
+
 //import Image from "next/image";
 import styles from "./page.module.css";
 
-import { Box, Text } from "@mantine/core";
 import NavBar from "./Components/NavBar";
 import Layout from "./Components/LandingPage/Layout";
 import Trusted from "./Components/Trusted";
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <NavBar />
-      <Layout />
+
       <Trusted />
     </main>
   );
